03988190c988950bdc1d3033299354b8188fd62b
Kilian <kguevarabarrios@gmail.com>
2022-06-02 15:01:32 -0400

    Modificado seccion_2\selects.js

--- cypress/integration/seccion_2/selects.js
+++ cypress/integration/seccion_2/selects.js
@@ -9,27 +9,34 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 describe("", () =>{
 
-    it("Selects Demo", () =>{
-        cy.visit("https://demo.anhtester.com/basic-select-dropdown-demo.html")
-        cy.title().should('eq','Selenium Easy Demo - Automate All Scenarios')
-        cy.get('#select-demo').should("be.visible").select("Monday").should("have.value","Monday")
-    })
+        it("Selects Demo", () =>{
+            cy.visit("https://demo.anhtester.com/basic-select-dropdown-demo.html")
+            cy.title().should('eq','Selenium Easy Demo - Automate All Scenarios')
+            cy.get('#select-demo').should("be.visible").select("Monday").should("have.value","Monday")
+        })
 
-    it("Multi Select", () =>{
-        cy.visit("https://demo.anhtester.com/basic-select-dropdown-demo.html")
-        cy.title().should('eq','Selenium Easy Demo - Automate All Scenarios')
-        cy.get("#multi-select").should("be.visible").select(["California","New York"])
-    })
+        it("Multi Select", () =>{
+            cy.visit("https://demo.anhtester.com/basic-select-dropdown-demo.html")
+            cy.title().should('eq','Selenium Easy Demo - Automate All Scenarios')
+            cy.get("#multi-select").should("be.visible").select(["California","New York"])
+        })
 
-    it.only("Multi Select Reto", () =>{
-        cy.visit("https://demo.anhtester.com/jquery-dual-list-box-demo.html")
-        cy.title().should('eq','Selenium Easy - JQuery Dual List Box Demo')
-        cy.get(':nth-child(1) > .form-control').select(["Isis","Manuela"]).then(()=>{
+        it("Multi Select Reto", () =>{
+            cy.visit("https://demo.anhtester.com/jquery-dual-list-box-demo.html")
+            cy.title().should('eq','Selenium Easy - JQuery Dual List Box Demo')
+            cy.get(':nth-child(1) > .form-control').select(["Isis","Manuela"]).then(()=>{
             cy.get('.pAdd').should("be.visible").click()
             cy.get(':nth-child(3) > .form-control').should("be.visible").select(["Isis","Manuela"])
             cy.get('.pRemove').should("be.visible").click()
         })
-                   
+
+                  
+    })
+
+    it.only("Selects Adicional", () =>{
+        cy.visit("https://testpages.herokuapp.com/styled/basic-html-form-test.html")
+        cy.title().should('eq','HTML Form Elements')
+        cy.get('[multiple="multiple"]').should("be.visible").select("ms2").should("contain","Selection Item 2")
     })
 
 })//Cierre de describe
